d7e9015126d96c383122cc8bb169b3b9b4604c4d
SlendyMcTendies <basecampx530@gmail.com>
2026-06-09 08:39:47 -0400

    SKSE Scanner Add (Incomplete)

--- src/NPCScanner.cpp
+++ src/NPCScanner.cpp
@@ -5,6 +5,74 @@
 #include "NPCData.h"
 #include "Logger.h"
 
+void ForEachReferenceInRange(RE::TESObjectREFR* origin, float radius, std::function<RE::BSContainer::ForEachResult(RE::TESObjectREFR& ref)> callback)
+{
+	if (origin && radius > 0.0f) {
+		const auto originPos = origin->GetPosition();
+		auto* tesSingleton = RE::TES::GetSingleton();
+		auto* interiorCell = tesSingleton->interiorCell;
+		if (interiorCell) {
+			interiorCell->ForEachReferenceInRange(originPos, radius, [&](RE::TESObjectREFR* a_ref) {
+				return a_ref ? callback(*a_ref) : RE::BSContainer::ForEachResult::kContinue;
+				});
+		}
+		else {
+			if (const auto gridLength = tesSingleton->gridCells ? tesSingleton->gridCells->length : 0; gridLength > 0) {
+				const float yPlus = originPos.y + radius;
+				const float yMinus = originPos.y - radius;
+				const float xPlus = originPos.x + radius;
+				const float xMinus = originPos.x - radius;
+
+				std::uint32_t x = 0;
+				do {
+					std::uint32_t y = 0;
+					do {
+						if (const auto cell = tesSingleton->gridCells->GetCell(x, y); cell && cell->IsAttached()) {
+							if (const auto cellCoords = cell->GetCoordinates(); cellCoords) {
+								const RE::NiPoint2 worldPos{ cellCoords->worldX, cellCoords->worldY };
+								if (worldPos.x < xPlus && (worldPos.x + 4096.0f) > xMinus && worldPos.y < yPlus &&
+									(worldPos.y + 4096.0f) > yMinus) {
+									cell->ForEachReferenceInRange(originPos, radius, [&](RE::TESObjectREFR* a_ref) {
+										return a_ref ? callback(*a_ref) : RE::BSContainer::ForEachResult::kContinue;
+										});
+								}
+							}
+						}
+						++y;
+					} while (y < gridLength);
+					++x;
+				} while (x < gridLength);
+			}
+		}
+	}
+	else {
+		RE::TES::GetSingleton()->ForEachReference([&](RE::TESObjectREFR* a_ref) {
+			return a_ref ? callback(*a_ref) : RE::BSContainer::ForEachResult::kContinue;
+			});
+	}
+}
+
+std::vector<RE::Actor*> GetActorsInRadiusOfPlayer(float radius)
+{
+	std::vector<RE::Actor*> nearbyActors;
+
+	if (!Player || !Player->parentCell) {
+		Log("<C++ ActorScanner> [GetActorsInRadiusOfPlayer] Warning: Source (Player or Player's Parent Cell) is null", LogType::Core, LoggingLevel::warning);
+		return nearbyActors;
+	}
+
+	ForEachReferenceInRange(Player, radius, [&](RE::TESObjectREFR& ref) {
+		auto refBase = ref.GetBaseObject();
+		auto actor = ref.As<RE::Actor>();
+		if (actor && actor != Player && !actor->IsDisabled() && !actor->IsDead()) {
+			nearbyActors.push_back(actor);
+		}
+		return RE::BSContainer::ForEachResult::kContinue;
+		});
+
+	return nearbyActors;
+}
+
 void ProcessNPC(RE::StaticFunctionTag*, RE::Actor* akActor, float CurrentGameTime, int SexualityScore) {
 	if (akActor == nullptr) {
 		Log("<C++ NPCScanner> [ProcessNPC] Received a NONE/Null Actor!", LogType::Core, LoggingLevel::error);
